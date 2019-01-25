4a878bfa798daff367169159b1e398c4bcba3e1e
gunnerdeath <jingkcheng@hotmail.com>
2019-01-25 13:39:46 -0500

    jing start

---
+++ Lab-3-unittest-test.js
@@ -0,0 +1,14 @@
+//LAB 3 - UNIT TESTING
+//======== THIS FILE IS FOR THE UNIT TEST ==========
+
+// Take 2 parameters
+// - A value to pass to the checkHumbrId function
+//  and the expected result of running that value with the function
+// - Output results of the test to the html
+
+function test_checkHumbrId(valueIn, expected){
+    "use strict";
+
+    
+
+}
